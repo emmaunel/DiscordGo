d718985da15eb3c106082b925618a32a595cc25f
d3adzo <enzo.destephano@gmail.com>
2021-12-08 13:50:34 -0500

    fixed import issues and first time setup bugs

---
+++ pkg/util/constants/variables.go
@@ -0,0 +1,11 @@
+package constants
+
+// agent variables
+var ServerID = "XXXXXXXXXXXXXXXXXX"
+var BotToken = "XXXXXXXXXXXXXXXXXX"
+
+
+// server variables
+var DBUsername = "XXXXXXXXXXXXXXXXXX"
+var DBPassword = "XXXXXXXXXXXXXXXXXX"
+var ChannelID = "XXXXXXXXXXXXXXXXXX"
